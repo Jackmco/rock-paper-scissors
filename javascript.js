cdb07ab35ae0d45e45c8ae2244e4f658f53ae60e
Jackmco <jmcohen99@gmail.com>
2024-10-19 00:18:10 -0500

    Reworked game to be built for a website UI

--- javascript.js
+++ javascript.js
@@ -14,20 +14,71 @@ function getComputerChoice() {
     return
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt("Rock, paper, or scissors?").toLowerCase()
-    if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
-       return humanChoice
+let roundCount = 0
+let computerWins = 0
+let humanWins = 0
+let buttons = document.querySelectorAll("button")
+for (let i=0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", () => {
+    var humanChoice = buttons.item(i).innerHTML
+    var computerChoice = getComputerChoice()
+    var roundResult = playRound(humanChoice, computerChoice)
+    if (roundCount < 1) { // create a div for the individual round result
+            var htmlRoundResult = Object.assign(document.createElement("div"), { id: 'roundResult' })
+            document.body.appendChild(htmlRoundResult)
+            var htmlComputerWins = Object.assign(document.createElement("div"), { id: 'computerWins' })
+            var htmlHumanWins = Object.assign(document.createElement("div"), { id: 'humanWins' })
+            document.body.appendChild(htmlRoundResult)
+            document.body.appendChild(htmlComputerWins)
+            document.body.appendChild(htmlHumanWins)
     }
-    else {
-        alert("That is not a permitted choice.")
-        getHumanChoice()   
+    if (roundResult === "human") {
+            humanWins++
+            document.getElementById("roundResult").textContent = "You win! " + humanChoice + " beats " + computerChoice + "."
+        } 
+        else if (roundResult === "computer") {
+            computerWins++
+            document.getElementById("roundResult").textContent = "You lose! " + computerChoice + " beats " + humanChoice + "."
+        }
+        else {
+            document.getElementById("roundResult").textContent = "It's a draw! You both picked " + humanChoice + "."
+        }
+        document.getElementById("computerWins").textContent = "Computer round wins: " + computerWins
+        document.getElementById("humanWins").textContent = "Human round wins: " + humanWins
+        roundCount++
+        console.log(humanWins, computerWins)
+    if (humanWins === 5 || computerWins === 5) {
+        var gameOver = Object.assign(document.createElement("div"), { id: 'gameOver' })
+        document.body.appendChild(gameOver)
+        for (let i=0; i < buttons.length; i++) {
+            buttons[i].disabled = true
+        }
+        if (humanWins === 5) {
+            document.getElementById("gameOver").textContent = "Congratulations, you win!"
+        }
+        else if (computerWins === 5) {
+            document.getElementById("gameOver").textContent = "Oh no! The computer wins."
+        }
     }
-}
+        })
+    }
+
+// Non-UI Code v---
+// function getHumanChoice() {
+//     let humanChoice = prompt("Rock, paper, or scissors?").toLowerCase()
+//     if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
+//        return humanChoice
+//     }
+//     else {
+//         alert("That is not a permitted choice.")
+//         getHumanChoice()   
+//     }
+// }
 
 function playRound(humanChoice, computerChoice) {
-    console.log(humanChoice, computerChoice)
-    let result
+    // console.log(humanChoice, computerChoice)
+    let result = ""
+    humanChoice = humanChoice.toLowerCase()
     if (humanChoice === "rock") {
         if (computerChoice === "rock") {
             result = "tie"        
@@ -64,26 +115,26 @@ function playRound(humanChoice, computerChoice) {
     return result
 }
 
-function playGame() {
-    for (let i = 1; i <= 5; i++) {
-        humanChoice = getHumanChoice()
-        computerChoice = getComputerChoice()
-        console.log("Result of Game " + i + ":")
-        result = playRound(humanChoice, computerChoice)
-        if (result === "human") {
-            ++humanScore
-            console.log("You win! " + humanChoice + " beats " + computerChoice + ".")
-        } 
-        else if (result === "computer") {
-            ++computerScore
-            console.log("You lose! " + computerChoice + " beats " + humanChoice + ".")
-        }
-        else {
-            console.log("It's a draw! You both picked " + humanChoice + ".")
-        }
-        console.log("Human games won: " + humanScore)
-        console.log("Computer games won: " + computerScore)
-   }
-}
+// function playGame() {
+//     for (let i = 1; i <= 5; i++) {
+//         humanChoice = getHumanChoice()
+//         computerChoice = getComputerChoice()
+//         console.log("Result of Game " + i + ":")
+//         result = playRound(humanChoice, computerChoice)
+//         if (result === "human") {
+//             ++humanScore
+//             console.log("You win! " + humanChoice + " beats " + computerChoice + ".")
+//         } 
+//         else if (result === "computer") {
+//             ++computerScore
+//             console.log("You lose! " + computerChoice + " beats " + humanChoice + ".")
+//         }
+//         else {
+//             console.log("It's a draw! You both picked " + humanChoice + ".")
+//         }
+//         console.log("Human games won: " + humanScore)
+//         console.log("Computer games won: " + computerScore)
+//    }
+// }
 
-playGame()
+// playRound()
